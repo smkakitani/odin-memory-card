ee5a0d7b1513fdef4081e26bcc2fc7810b96f685
smkakitani <82690252+kakisaura@users.noreply.github.com>
2023-10-22 23:31:49 -0300

    Organize functions

--- src/components/Generation.jsx
+++ src/components/Generation.jsx
@@ -1,31 +1,7 @@
-import { useState, useEffect } from "react";
-import fetchData from "./PokemonApi";
+import '../styles/Generation.css'
 
 
-
-export default function GenerationInput() {
-  // const generationsList = ['generation-i', 'generation-ii', 'generation-iii', 'generation-iv', 'generation-v', 'generation-vi', 'generation-vii', 'generation-viii', 'generation-ix'];  
-  const [generationList, setGenerationList] = useState([]);
-
-  // Populate an array with API's generation data 
-  useEffect(() => {
-    let ignore = false
-
-    fetchData('generation').then(result => {
-      if(!ignore) {
-        console.log('Fetched generation list from useEffect');
-        let nextId = 1;
-        const tempArray = [];
-        result.results.forEach(gen => {
-          tempArray.push({ id: nextId++, name: gen.name });
-        });
-        // console.log(tempArray);
-        setGenerationList(tempArray);
-      }
-    });
-
-    return () => ignore = true
-  }, []);
+export default function GenerationBox({ generationList, handleRadio }) {
 
   // Fix name from API's data to be able to use it as input text
   function manageGenerationName(text) {
@@ -37,23 +13,32 @@ export default function GenerationInput() {
     return newText;
   }
 
-  const createSelectGeneration = generationList.map(generation =>
-    <label key={generation.id}>
-      <input
-      type="radio" 
-      name="generation" 
-      value={generation.name}
-      />
-      {manageGenerationName(generation.name)}
-    </label>
+  // Use array -rowGenerations- to insert the component with data fetched from API
+  const rowGenerations = [];
+
+  generationList.map(generation =>
+    rowGenerations.push(
+      <label key={generation.id}>
+        <input
+        type="radio" 
+        name="generation" 
+        value={generation.name}
+        onChange={handleRadio}
+        defaultChecked={generation.name === 'generation-i'}
+        />
+        <span className="material-symbols-outlined">
+          arrow_right
+          </span>
+        {manageGenerationName(generation.name)}
+      </label>
+    )
   );
-  // console.log(generationList.length);
 
   return (
     <fieldset>
-      <legend>Select your preferred Pokemon generation:</legend>
-      <div>
-        {createSelectGeneration}
+      <div className='legend'>Select your preferred Pokemon generation:</div>
+      <div className='generationList'>
+        {rowGenerations}
       </div>
     </fieldset>
   );
