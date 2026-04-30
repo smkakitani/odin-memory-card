f9cb2d34c13a543c1e377886ca953bba2e1927b6
smkakitani <82690252+kakisaura@users.noreply.github.com>
2026-04-30 11:52:35 -0300

    Rename file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+// Styles
+import '../styles/Footer.css';
+
+
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        Copyright © 2023 
+        <a 
+          href="https://github.com/smkakitani/odin-memory-card" 
+          target='_blank' 
+          rel='noreferrer'
+        >
+          <img 
+            height="25" 
+            src="/github-mark-white.svg" 
+            alt="Github Icon." 
+          />
+        </a>
+      </div>
+    </footer>
+  );
+}
